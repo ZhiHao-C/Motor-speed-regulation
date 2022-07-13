cc468af1b09640b0f581ecaa672c9d1057c4d69e
微风轻轻起 <108532636+ZhiHao-C@users.noreply.github.com>
2022-07-13 17:08:06 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,15 @@
 #include "delay.h"
 #include "shumaguan.h"
 #include "keycode.h"
-#include "IR.h"
-//sbit led=P2^0;
 unsigned char keynum;
-unsigned char num;
 unsigned char c,compare;
-unsigned char address;
-unsigned char command;
-unsigned char dataflag;
-void timing_1init()
+void timing_1init()             //用定时器1初始化
 {
 	TMOD=0X10;
 	TF1=0;
 	EA=ET1=1;
-	TL1 = 0xB4;		//���ö�ʱ��ֵ
-	TH1 = 0xFE;		//���ö�ʱ��ֵ
+	TL1 = 0xB4;		//设置定时初值
+	TH1 = 0xFE;		//设置定时初值
 	TR1 =1;
 }
 
@@ -24,7 +18,7 @@ void timing_1init()
 void main()
 {
 	
-	compare=100;
+	compare=100;            //初始先在第一个数码管显示0并调节占空比为0
 	display(1,0);
 	timing_init();
 	while(1)
@@ -32,7 +26,7 @@ void main()
 		keynum=key();
 		if(keynum)
 		{
-			if(keynum==1)
+			if(keynum==1)//按键1按下占空比为1/4
 			{
 				compare=75;
 				display(1,keynum);
@@ -59,69 +53,19 @@ void main()
 
 
 
-//void main()
-//{
-//	
-//	timing_1init();
-//	timing_0init();
-//	outinterrupt_init();
-//	compare=100;
-//	display(1,0);
-//	while(1)
-//	{
-//		if(IR_getdataflag())
-//		{
-//			address=getaddress();
-//			command=getcommand();
-//			if(command==0x0c)
-//			{
-//				compare=75;
-//				display(1,1);
-//			}
-//			else if(command==0x18)
-//			{
-//				compare=50;
-//				display(1,2);
-//			}
-//			else if(command==0x5e)
-//			{
-//				compare=20;
-//				display(1,3);
-//			}
-//			else if(command==0x08)
-//			{
-//				compare=0;
-//				display(1,4);
-//			}
-//		}
-//	}
-//	
-//	
-//	
-//}
-
-
-
-
-
-
-
-
 
 void timing()interrupt 3
 {
-	TL1 = 0xB4;		//���ö�ʱ��ֵ
-	TH1 = 0xFE;		//���ö�ʱ��ֵ
-	c++;
-	c=c%100;
-	if(c<compare)
+	TL1 = 0xB4;		//设置定时初值
+	TH1 = 0xFE;		//设置定时初值
+	c++;                    //每次中断加1
+	c=c%100;                //c为100清零
+	if(c<compare)           //调节占空比
 	{
 		P1_0=0;
-		led=0;
 	}
 	else if(c>=compare)
 	{
 		P1_0=1;
-		led=1;
 	}
 }
